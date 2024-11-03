1a93b27cebbcd7340134bf837ecbb78c9222e1bd
FacuLazzarino <lazzarinofacu@gmail.com>
2024-11-03 18:52:16 -0300

    boton y negritas
    se terminó la página poniendo el botón de scroll up y negritas y retoques en descripciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-function myFunction() {
+
+  function myFunction() {
     let x = document.getElementById("menubarra");
     if (x.className === "barratop") {
       x.className += " responsive";
@@ -6,4 +7,26 @@ function myFunction() {
       x.className = "barratop";
     }
   }
-  
+  function personajes() {
+    let y = document.getElementById("sub1");
+    if (y.className === "per") {
+      y.className += " responsive";
+    } else {
+      y.className = "per";
+    }
+  }
+  function episodios() {
+    let z = document.getElementById("sub2");
+    if (z.className === "epi") {
+      z.className += " responsive";
+    } else {
+      z.className = "epi";
+    }
+  }
+  $(window).scroll(function(){
+    if($(window).scrollTop()>50){
+      $('.irarriba').addClass('nueva-clase');
+    } else{
+      $('.irarriba').removeClass('nueva-clase')
+    }
+  });
